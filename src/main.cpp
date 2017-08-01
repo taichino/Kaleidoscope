50815b11347b49eecc06138588db8a2bb011018b
taichino <taichino@gmail.com>
2017-08-01 18:14:24 -0400

    Token

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,22 @@
 
 using namespace std;
 
+
+enum Token {
+    tok_eof = -1,
+
+    // commands
+    tok_def = -2,
+    tok_extern = -3,
+
+    // primary
+    tok_identifier = -4,
+    tok_number = -5,
+};
+
+static string IdentifierStr;
+static double NumVal;
+
 int main() {
     cout << "hello world" << endl;
     return 0;
